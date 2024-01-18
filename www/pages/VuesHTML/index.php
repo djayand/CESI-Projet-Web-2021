0735a3c45b96a737159e53bd4b4323801ede0bda
Djay <djayan.demaison@live.fr>
2024-01-18 15:05:45 +0100

    improve front

--- www/pages/VuesHTML/index.php
+++ www/pages/VuesHTML/index.php
@@ -5,21 +5,30 @@ session_start();
 <!DOCTYPE html>
 <html lang="fr">
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
 <head>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script src="../../ressources/js/app.js"></script>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>Page d'acceuil</title>
     <link rel="stylesheet" type="text/css" href="../../ressources/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="../../ressources/css/style.css">
-    <link rel="apple-touch-icon" href="../../ressources/img/icon96x96.jpg">
-    <link rel="manifest" href="../../ressources/manifest.json">
-    <meta name="apple-mobile-web-app-status-bar" content="white">
-    <meta name="theme-color" content="#FFFF" />
+    <link rel="stylesheet" type="text/css" href="../../ressources/css/style.css">
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Colorlib Templates">
+    <meta name="author" content="Colorlib">
+    <meta name="keywords" content="Colorlib Templates">
+    <!-- Icons font CSS-->
+    <link href="../../ressources/colorlib-regform-3/vendor/mdi-font/css/material-design-iconic-font.min.css"
+        rel="stylesheet" media="all">
+    <link href="../../ressources/colorlib-regform-3/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet"
+        media="all">
+    <!-- Font special for pages-->
+    <link
+        href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <!-- Vendor CSS-->
+    <link href="../../ressources/colorlib-regform-3/vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="../../ressources/colorlib-regform-3/vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+    <!-- Main CSS-->
+    <link href="../../ressources/colorlib-regform-3/css/main.css" rel="stylesheet" media="all">
 </head>
 
 <header>
@@ -27,31 +36,39 @@ session_start();
 </header>
 
 <body>
-    <main>
-        <section>
-            <h1 class="grand_titre">STAGE</h1>
-            <div class="aspect_btn">
-                <input class="btn btn-primary" type="button" onclick="window.location.href = 'Login.php';" value="Commencer" />
+    <div class="page-wrapper bg-gra-01 p-t-180 p-b-100 font-poppins">
+        <div class="wrapper wrapper--w780">
+            <div class="card card-3">
+                <div class="card-body">
+                    <h2 class="title">CESI-LDH</h2>
+                    <div class="container">
+                        <h4 class="title"> Bienvenue sur LDH, l'outil CESI de recherche de stage !</h2>
+                            <h6 class="title">
+                                Sur notre plateforme, tu pourras rechercher des entreprises et des stages partenaires de
+                                l'école !
+                                Tu pourras également importer tes CV et tes lettres de motivation..</h3>
+                                <input class="btn btn-primary" type="button"
+                                    onclick="window.location.href = 'Menu.php';" value="Commencer" />
+                    </div>
+
+                </div>
             </div>
-        </section>
+        </div>
+    </div>
 
-        <article class="bordure">
-            <h2 class="moyen_titre">Le Projet</h2>
-            <p class="col-xs-4 col-sm-4 col-md-12 col-lg-12 texte">
+    <!-- Jquery JS-->
+    <script src="../../ressources/colorlib-regform-3/vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="../../ressources/colorlib-regform-3/vendor/select2/select2.min.js"></script>
+    <script src="../../ressources/colorlib-regform-3/vendor/datepicker/moment.min.js"></script>
+    <script src="../../ressources/colorlib-regform-3/vendor/datepicker/daterangepicker.js"></script>
+    <!-- Main JS-->
+    <script src="../../ressources/colorlib-regform-3/js/global.js"></script>
 
-                Ciliciam vero, quae Cydno amni exultat, Tarsus nobilitat, urbs perspicabilis hanc condidisse Perseus memoratur, Iovis filius et Danaes, vel certe ex Aethiopia profectus Sandan quidam nomine vir opulentus et nobilis et Anazarbus auctoris vocabulum referens, et Mopsuestia vatis illius domicilium Mopsi, quem a conmilitio Argonautarum cum aureo vellere direpto redirent, errore abstractum delatumque ad Africae litus mors repentina consumpsit, et ex eo cespite punico tecti manes eius heroici dolorum varietati medentur plerumque sospitales.<br>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 
-                Pandente itaque viam fatorum sorte tristissima, qua praestitutum erat eum vita et imperio spoliari, itineribus interiectis permutatione iumentorum emensis venit Petobionem oppidum Noricorum, ubi reseratae sunt insidiarum latebrae omnes, et Barbatio repente apparuit comes, qui sub eo domesticis praefuit, cum Apodemio agente in rebus milites ducens, quos beneficiis suis oppigneratos elegerat imperator certus nec praemiis nec miseratione ulla posse deflecti.<br>
-
-                Vita est illis semper in fuga uxoresque mercenariae conductae ad tempus ex pacto atque, ut sit species matrimonii, dotis nomine futura coniunx hastam et tabernaculum offert marito, post statum diem si id elegerit discessura, et incredibile est quo ardore apud eos in venerem uterque solvitur sexus.<br><br>
-            </p>
-        </article>
-    </main>
-
-    <footer>
-        <?php include('footer.php'); ?>
-    </footer>
-
-</body>
+<footer>
+    <?php include('footer.php'); ?>
+</footer>
 
 </html>
