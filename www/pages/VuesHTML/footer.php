b0971d5e81a6313d5898d83ae6640fd48e5cd4b2
Djay <djayan.demaison@live.fr>
2024-01-18 15:22:28 +0100

    improve footer

--- www/pages/VuesHTML/footer.php
+++ www/pages/VuesHTML/footer.php
@@ -1,7 +1,10 @@
 <link rel="stylesheet" type="text/css" href="../../ressources/css/stylefh.css">
 
-<body>
-<div class=ftr>
-Le site respecte toutes les mentions légales. 2020-2021 Copyright tout droits réservés
-	</div>
-</body>
+<div class="ftr">
+    <p>© 2020-2021, Tous droits réservés - 
+        <a href="mention.php">Mentions légales</a> | 
+        <a href="help.php">Politique de confidentialité</a>
+        <a href="https://www.linkedin.com" class="social-icon"><img class="social-icon" src="https://assets.stickpng.com/images/6297a2f1e01809629f113598.png" alt="LinkedIn"></a>
+        <a href="https://www.facebook.com" class="social-icon"><img class="social-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/480px-Facebook_f_logo_%282019%29.svg.png" alt="Facebook"></a>
+    </p>
+</div>
