dbaefe3215998724099d316f5636aa48a7f3fd59
Djay <djayan.demaison@live.fr>
2024-01-18 15:27:02 +0100

    improve header

--- www/pages/VuesHTML/header.php
+++ www/pages/VuesHTML/header.php
@@ -4,25 +4,23 @@
 	<div class=hdr>
 		<a href="index.php">Acceuil</a>
 
-		<?php 
-		if (!isset($_COOKIE['pseudo']))
-		{
-		echo '<a href="login.php">Connexion</a>';
-		echo '<a href="inscription.php">Inscription</a>';}
-		else
-		{echo '<a href="../Modeles/M_Unlogin.php">Se déconnecter</a>';}
-		
-		if (isset($_COOKIE['pseudo']))
-		{
-		echo '<a href="Menu.php">Menu</a>';
-		echo '<a href="stage.php">Stages</a>';
-		echo '<a href="entreprise.php">Entreprises</a>';
-		echo '<a href="membre.php">Membres</a>';
-		echo '<a href="mesLM.php">Mes lettres</a>';
-		echo '<a href="MesCV.php">Mes CV</a>';
+		<?php
+		if (!isset($_COOKIE['pseudo'])) {
+			echo '<a href="login.php">Connexion</a>';
+			echo '<a href="inscription.php">Inscription</a>';
+		} else {
+			echo '<a href="../Modeles/M_Unlogin.php">Se déconnecter</a>';
+		}
+
+		if (isset($_COOKIE['pseudo'])) {
+			echo '<a href="Menu.php">Menu</a>';
+			echo '<a href="stage.php">Stages</a>';
+			echo '<a href="entreprise.php">Entreprises</a>';
+			echo '<a href="membre.php">Membres</a>';
+			echo '<a href="MesCV.php">Mes CV</a>';
+			echo '<a href="mesLM.php">Mes lettres</a>';
 		}
 		?>
-		<a href="mention.php">Mentions Légales</a>
 	</div>
 
 	<div class=hdr2>
